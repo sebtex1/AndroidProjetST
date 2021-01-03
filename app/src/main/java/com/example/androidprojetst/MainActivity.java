1bcb5244be52017950b16c24feab01a15f0834a0
Thomas FERREIRA <thomas.ferreira@kafein-studio.fr>
2021-01-03 15:58:02 +0100

    creation de la 1ere page

--- app/src/main/java/com/example/androidprojetst/MainActivity.java
+++ app/src/main/java/com/example/androidprojetst/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.androidprojetst;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Timer mytimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mytimer = new Timer();
+        mytimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent myIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(myIntent);
+                finish();
+            }
+        },2000);
     }
 }
