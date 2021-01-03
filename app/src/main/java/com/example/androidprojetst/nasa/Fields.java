1f7f36c626779fc89b9ba8ca29d6ad97413be670
SebTex <36498890+sebtex1@users.noreply.github.com>
2021-01-03 17:59:00 +0100

    ImageActivity

---
+++ app/src/main/java/com/example/androidprojetst/nasa/Fields.java
@@ -0,0 +1,42 @@
+package com.example.androidprojetst.nasa;
+
+import java.util.Date;
+
+public class Fields {
+    private Date date;
+    private String explanation;
+    private String title;
+    private String urlImage;
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrlImage() {
+        return urlImage;
+    }
+
+    public void setUrlImage(String urlImage) {
+        this.urlImage = urlImage;
+    }
+}
---
+++ app/src/main/java/com/example/androidprojetst/utils/Constant.java
@@ -0,0 +1,6 @@
+package com.example.androidprojetst.utils;
+
+public class Constant {
+
+    public static final String URL = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+}
